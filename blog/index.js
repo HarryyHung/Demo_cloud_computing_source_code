6c66252d2c8b6c41830228362dbb090ac1c6934c
HungBuii <buiduyhungg2310@gmail.com>
2022-12-21 10:51:57 +0700

    Update 5 (index.js)
    npm i morgan --save-dev

--- blog/index.js
+++ blog/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express() // tra ve doi tuong web
 const port = 3000 // cong web muon run
 
+app.use(morgan('combined'))
+
 // route
 app.get('/', (req, res) => {
-    var a = 1;
-    var b = 2;
-    var c = a + b;
   res.send('Hello World!')
 })
 
