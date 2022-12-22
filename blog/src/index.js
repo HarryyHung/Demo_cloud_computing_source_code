02f0623c3db2c0959f1519f93b4193b2414397d1
HungBuii <buiduyhungg2310@gmail.com>
2022-12-23 02:59:01 +0700

    Update 11
    https://expressjs.com/en/starter/basic-routing.html
    
    Update search.hbs

--- blog/src/index.js
+++ blog/src/index.js
@@ -29,23 +29,28 @@ const port = 3000
 app.use(express.static(path.join(__dirname, 'public'))); // luu anh (thu muc tinh)
 
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 //template engine
 app.engine('hbs', handlebars.engine({
-    extname: '.hbs'
+  extname: '.hbs'
 }));
-app.set('view engine','hbs')
+app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resources/views'));
 console.log(path.join(__dirname, 'resources/views'))
 
 app.get('/', (req, res) => {
   res.render('home')
-})
+});
 
 app.get('/news', (req, res) => {
+  console.log(req.query.q);
   res.render('news')
-})
+});
+
+app.get('/search', (req, res) => {
+  res.render('search')
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
