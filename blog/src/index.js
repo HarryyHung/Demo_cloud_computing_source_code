791e4441422a902b1e11768c1c0ef8b050d88915
HungBuii <buiduyhungg2310@gmail.com>
2022-12-22 15:38:16 +0700

    Update 7

--- blog/src/index.js
+++ blog/src/index.js
@@ -1,20 +1,44 @@
-const express = require('express');
-const morgan = require('morgan');
-const handlebars  = require('express-handlebars');
-const app = express(); // tra ve doi tuong web
-const port = 3000; // cong web muon run
+// const express = require('express');
+// const morgan = require('morgan');
+// const handlebars  = require('express-handlebars');
+// const app = express(); // tra ve doi tuong web
+// const port = 3000; // cong web muon run
 
-// HTTP logger
-app.use(morgan('combined'));
+// // HTTP logger
+// app.use(morgan('combined'));
+
+// //template engine
+// app.engine('handlebars', handlebars.engine());
+// // app.set('view engine','handlebars')
+// // app.set('views', path.join(__dirname, 'resources', 'views'));
+// app.set('views', './src/resources/views')
+
+// // route
+// app.get('/', (req, res) => {
+//     res.render('home');
+// })
+
+const path = require('path')
+const express = require('express')
+const app = express()
+const handlebars = require('express-handlebars')
+const { extname } = require('path')
+const port = 3000
 
 //template engine
-app.engine('handlebars', handlebars.engine());
-app.set('view engine','handlebars')
-app.set('views', path.join(__dirname, 'resources', 'views'));
+app.engine('hbs', handlebars.engine({
+    extname: '.hbs'
+}));
+app.set('view engine','hbs')
+app.set('views', path.join(__dirname, 'resources/views'));
+console.log(path.join(__dirname, 'resources/views'))
 
-// route
 app.get('/', (req, res) => {
-    res.render('home');
+  res.render('home')
+})
+
+app.get('/news', (req, res) => {
+  res.render('news')
 })
 
 app.listen(port, () => {
